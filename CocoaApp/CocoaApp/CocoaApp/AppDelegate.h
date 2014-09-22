ad255ae11b03613f44dff62c03af8215c25326fc
Thomas Albrecht-Beste <thomas@beste.dk>
2014-09-22 13:05:36 +0200

    Initial Commit

---
+++ CocoaApp/CocoaApp/CocoaApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaApp
+//
+//  Created by Thomas Albrecht-Beste on 22/09/14.
+//  Copyright (c) 2014 Thomas Albrecht-Beste. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CocoaApp/CocoaApp/CocoaApp/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CocoaApp
+//
+//  Created by Thomas Albrecht-Beste on 22/09/14.
+//  Copyright (c) 2014 Thomas Albrecht-Beste. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
